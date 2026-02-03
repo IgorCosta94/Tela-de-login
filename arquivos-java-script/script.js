df001bca1af7f567e79544dd1ea8a85e42e9e124
IgorCosta94 <igorcosta_94@hotmail.com>
2026-02-03 08:58:48 -0300

    Trabalhando com eventos no java script(getElementById,querySelector,addEvenListener

--- arquivos-java-script/script.js
+++ arquivos-java-script/script.js
@@ -1,1 +1,22 @@
-alert("Bem-vindo à tela de login da Ecotech Solution!");
+const email = "igor@hotmail.com";
+
+console.log(`seu email é: ${email}`);
+console.log("seu email é:  email");
+
+document.getElementById("botao").addEventListener("click", e => {
+    console.log("O botão foi clicado!");
+});
+
+document.getElementById("form-login").addEventListener("mouseenter", e => {
+    console.log("O mause está sobre o formulário!");
+    
+});
+
+document.querySelector("#form-login").addEventListener("mouseleave", e => {
+    console.log("O mause saiu de cima do formulário!");
+});
+
+document.querySelector("#form-login").addEventListener("submit", e => {
+    e.preventDefault();
+    console.log("Formulário enviado");
+});
